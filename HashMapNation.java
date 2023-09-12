42936b4f05055ac1d5b81fc99b85c7a06d18aad2
daehanLee <daehanl8635@gmail.com>
2023-09-12 20:51:28 +0900

    complete

--- HashMapNation.java
+++ HashMapNation.java
@@ -7,13 +7,13 @@ public class HashMapNation {
 		String stop = "그만";
 		System.out.println("나라 이름과 인구를 10개 입력하세요.(예: Korea 5000)");
 		while(true) { // "그만"이 입력될 때까지 반복
-			if(nations.containsKey("그만")) {
-				break;
-			}
 			System.out.print("나라 이름, 인구 >> ");
 			
 			
 			String nationName = scanner.next(); //나라이름
+			if(nationName.equals("그만")) {
+				break;
+			}
 			int population = scanner.nextInt(); //인구
 			
 			nations.put(nationName, population); // 나라이름, 인구 입력받은것을 HashMap에 추가
@@ -25,7 +25,7 @@ public class HashMapNation {
 			System.out.print("인구 검색 >> ");
 			
 			String nationName = scanner.next();
-			if(nationName == "그만") {
+			if(nationName.equals("그만")) {
 				break;
 			}
 			
